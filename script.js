4cc99a277099b4b0b251b0b3427f71fb2bb60a42
Skyramopdemo <henriaue.g.a@gmail.com>
2024-07-23 23:18:20 -0300

    CreateRoute e nearbySearh, apenas na origem adicionados.

--- script.js
+++ script.js
@@ -3,23 +3,21 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.set('views', path.join(__dirname, 'views'));
 
 const port = process.env.PORT; 
-
-
+const hostname = process.env.HOST;
 
 app.get('/', (req, res) => {
-  //res.send(process.env.API_KEY)
   const data = {
-    api: process.env.API_KEY
+    map_api: process.env.MAP_API_KEY,
+    route_api: process.env.ROUTE_API_KEY,
+    places_api: process.env.PLACES_API_KEY
   };
   res.render('index', data);
 })
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}/`);
-});
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); 
